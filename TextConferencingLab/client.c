092ed039ff2c6420c943d78570c868e0db3391d6
Zhang Qianhao <zhan5612@teach.cs.toronto.edu>
2017-11-03 23:11:44 -0400

    Filled in details for client.c

--- TextConferencingLab/client.c
+++ TextConferencingLab/client.c
@@ -7,13 +7,12 @@
 #include <pthread.h>
 #include <stdbool.h>
 
-#define BUF_SIZE 1024
-
+#include "packet.h"
 #define INVALID_SOCKET - 1
 // buffer for send and receive
 char buf[BUF_SIZE];
-// whether there is a listener thread
-bool pthread = false;
+// whether user is in a session
+bool insession = false;
 // supported commands
 const char *LOGIN_CMD = "/login";
 const char *LOGOUT_CMD = "/logout";
@@ -24,16 +23,34 @@ const char *LIST_CMD = "/list";
 const char *QUIT_CMD = "/quit";
 
 void *receive(void *socketfd_void_p) {
-	int *socketfd_p = (int *)socketfd_void_p;
+	// receive may get: JN_ACK, JN_NAC, NS_ACK, QU_ACK, MESSAGE
+  // only LOGIN packet is not listened by receive
+  int *socketfd_p = (int *)socketfd_void_p;
 	int numbytes;
-	for (;;) {
-		
+	Packet packet;
+  for (;;) {	
 		if ((numbytes = recv(*socketfd_p, buf, BUF_SIZE - 1, 0)) == -1) {
 			fprintf(stderr, "client receive: recv\n");
 			return NULL;
 		}
-		buf[numbytes] = '\0';
-		printf("client: received '%s'\n", buf);
+		stringToPacket(buf, &packet);
+    if (packet.type == JN_ACK) {
+      fprintf(stdout, "Successfully joined session %s.\n", packet.data);
+      insession = true;
+    } else if (packet.type == JN_NAK) {
+      fprintf(stdout, "Join failure. Detail: %s\n", packet.data);
+      insession = false;
+    } else if (packet.type == NS_ACK) {
+      fprintf(stdout, "Successfully created session %s.\n", packet.data);
+      insession = true;
+    } else if (packet.type == QU_ACK) {
+      fprintf(stdout, "Session #\tUser_ids\n%s", packet.data);
+    } else if (packet.type == MESSAGE){   
+      fprintf(stdout, "%s: %s", packet.source, packet.data);
+    } else {
+      fprintf(stdout, "Unexpected packet received: type %d, data %s\n",
+          packet.type, packet.data);
+    }
 	}
 	return NULL;
 }
@@ -46,7 +63,7 @@ void *get_in_addr(struct sockaddr *sa) {
     return &(((struct sockaddr_in6*)sa)->sin6_addr);
 }
 
-void login(char *pch, int *socketfd_p) {
+void login(char *pch, int *socketfd_p, pthread_t *receive_thread_p) {
 	char *client_id, *password, *server_ip, *server_port;
 	// tokenize the command
 	pch = strtok(NULL, " ");
@@ -97,20 +114,42 @@ void login(char *pch, int *socketfd_p) {
 		freeaddrinfo(servinfo); // all done with this structure
 
 		int numbytes;
-		// TODO: content of send
-		if ((numbytes = send(*socketfd_p, "login", BUF_SIZE - 1, 0)) == -1) {
+    Packet packet;
+    packet.type = LOGIN;
+    strncpy(packet.source, client_id, MAX_NAME);
+    strncpy(packet.data, password, MAX_DATA);
+    packet.size = strlen(packet.data);
+    packetToString(&packet, buf);
+		if ((numbytes = send(*socketfd_p, buf, BUF_SIZE - 1, 0)) == -1) {
 			fprintf(stderr, "client: send\n");
-			*socketfd_p = INVALID_SOCKET;
+			close(*socketfd_p);
+      *socketfd_p = INVALID_SOCKET;
 			return;
 		}
 
 		if ((numbytes = recv(*socketfd_p, buf, BUF_SIZE - 1, 0)) == -1) {
 			fprintf(stderr, "client: recv\n");
-			*socketfd_p = INVALID_SOCKET;
+			close(*socketfd_p);
+      *socketfd_p = INVALID_SOCKET;
 			return;
 		}
-		buf[numbytes] = '\0';
-    	printf("client: received '%s'\n", buf);
+    
+    stringToPacket(buf, &packet);
+    if (packet.type == LO_ACK && 
+        pthread_create(receive_thread_p, NULL, receive, socketfd_p) == 0) {
+      fprintf(stdout, "login successful.\n");  
+    } else if (packet.type == LO_NAK) {
+      fprintf(stdout, "login failed. Detail: %s\n", packet.data);
+      close(*socketfd_p);
+      *socketfd_p = INVALID_SOCKET;
+			return;
+    } else {
+      fprintf(stdout, "Unexpected packet received: type %d, data %s\n", 
+          packet.type, packet.data);
+      close(*socketfd_p);
+      *socketfd_p = INVALID_SOCKET;
+			return;
+    } 
 	}
 }
 
@@ -121,32 +160,33 @@ void logout(int *socketfd_p, pthread_t *receive_thread_p) {
 	} 
 
 	int numbytes;
-	// TODO: content of send
-	if ((numbytes = send(*socketfd_p, "logout", BUF_SIZE - 1, 0)) == -1) {
+  Packet packet;
+  packet.type = EXIT;
+  packet.size = 0;
+  packetToString(&packet, buf);
+	
+	if ((numbytes = send(*socketfd_p, buf, BUF_SIZE - 1, 0)) == -1) {
 		fprintf(stderr, "client: send\n");
 		return;
-	}
-	if (pthread) {
-		if (pthread_cancel(*receive_thread_p)) {
+	}	
+	if (pthread_cancel(*receive_thread_p)) {
 			fprintf(stderr, "logout leavesession failure\n");
-		} else {
-			fprintf(stdout, "logout leavesession success\n");
-			pthread = false;
-		} 
-	}
-
+	} else {
+			fprintf(stdout, "logout leavesession success\n");	
+	} 
+  insession = false;
 	close(*socketfd_p);
 	*socketfd_p = INVALID_SOCKET;
 }
 
-void joinsession(char *pch, int *socketfd_p, pthread_t *receive_thread_p) {
+void joinsession(char *pch, int *socketfd_p) {
 	if (*socketfd_p == INVALID_SOCKET) {
 		fprintf(stdout, "You have not logged in to any server.\n");
 		return;
-	} else if (pthread) {
-		fprintf(stdout, "You have already joined a session.\n");
-		return;
-	} 
+	} else if (insession) {
+    fprintf(stdout, "You have already joined a session.\n");
+    return;
+  }
 
 	char *session_id;
 	pch = strtok(NULL, " ");
@@ -155,104 +195,78 @@ void joinsession(char *pch, int *socketfd_p, pthread_t *receive_thread_p) {
 		fprintf(stdout, "usage: /joinsession <session_id>\n");
 	} else {
 		int numbytes;
-		// TODO: content of send
-		if ((numbytes = send(*socketfd_p, "joinsession", BUF_SIZE - 1, 0)) == -1) {
+    Packet packet;
+    packet.type = JOIN;
+    strncpy(packet.data, session_id, MAX_DATA);
+    packet.size = strlen(packet.data);
+
+		if ((numbytes = send(*socketfd_p, buf, BUF_SIZE - 1, 0)) == -1) {
 			fprintf(stderr, "client: send\n");
 			return;
-		}
-
-		if ((numbytes = recv(*socketfd_p, buf, BUF_SIZE - 1, 0)) == -1) {
-			fprintf(stderr, "client: recv\n");
-			return;
-		}
-		buf[numbytes] = '\0';
-		printf("client: received '%s'\n", buf);
-
-		if(pthread_create(receive_thread_p, NULL, receive, socketfd_p)) {
-			fprintf(stderr, "Error creating thread\n");
-			return;
-		} else {
-			fprintf(stdout, "Successfully creating thread\n");
-			pthread = true;
-		}
-
+		}	
 	}
 }
 
-void leavesession(int socketfd, pthread_t *receive_thread_p) {
+void leavesession(int socketfd) {
 	if (socketfd == INVALID_SOCKET) {
 		fprintf(stdout, "You have not logged in to any server.\n");
 		return;
-	} else if (!pthread) {
-		fprintf(stdout, "You have not joined a session yet.\n");
-		return;
-	}
+	} else if (!insession) {
+    fprintf(stdout, "You have not joined a session yet.\n");
+    return;
+  }
 
 	int numbytes;
-	// TODO: content of send
-	if ((numbytes = send(socketfd, "leavesession", BUF_SIZE - 1, 0)) == -1) {
+	Packet packet;
+  packet.type = LEAVE_SESS;
+  packet.size = 0;
+  packetToString(&packet, buf);
+	if ((numbytes = send(socketfd, buf, BUF_SIZE - 1, 0)) == -1) {
 		fprintf(stderr, "client: send\n");
 		return; 
 	}
-	if (pthread_cancel(*receive_thread_p)) {
-		fprintf(stderr, "leavesession failure\n");
-	} else {
-		fprintf(stdout, "leavesession success\n");
-		pthread = false;
-	} 
-
+  insession = false;
 }
 
-void createsession(char *pch, int *socketfd_p, pthread_t *receive_thread_p) {
+void createsession(char *pch, int *socketfd_p) {
 	if (*socketfd_p == INVALID_SOCKET) {
 		fprintf(stdout, "You have not logged in to any server.\n");
 		return;
-	} else if (pthread) {
-		fprintf(stdout, "You have already joined a session.\n");
-		return;
-	} 
+	} else if (insession) {
+    fprintf(stdout, "You have already joined a session.\n");
+    return;
+  }
 
 	char *session_id;
 	pch = strtok(NULL, " ");
 	session_id = pch;
 	if (session_id == NULL) {
-		fprintf(stdout, "usage: /joinsession <session_id>\n");
+		fprintf(stdout, "usage: /createsession <session_id>\n");
 	} else {
 		int numbytes;
-		// TODO: content of send
-		if ((numbytes = send(*socketfd_p, "createsession", BUF_SIZE - 1, 0)) == -1) {
+    Packet packet;
+    packet.type = NEW_SESS;
+    packet.size = 0;
+    packetToString(&packet, buf);		
+		if ((numbytes = send(*socketfd_p, buf, BUF_SIZE - 1, 0)) == -1) {
 			fprintf(stderr, "client: send\n");
 			return; 
 		}
-
-		if ((numbytes = recv(*socketfd_p, buf, BUF_SIZE - 1, 0)) == -1) {
-			fprintf(stderr, "client: recv\n");
-			return; 
-		}
-		buf[numbytes] = '\0';
-		printf("client: received '%s'\n", buf);
-
-		if(pthread_create(receive_thread_p, NULL, receive, socketfd_p)) {
-			fprintf(stderr, "Error creating thread\n");
-			return;
-		} else {
-			fprintf(stdout, "Successfully creating thread\n");
-			pthread = true;
-		}
-	}
+  }
 }
 
 void list(int socketfd) {
 	if (socketfd == INVALID_SOCKET) {
 		fprintf(stdout, "You have not logged in to any server.\n");
 		return;
-	} else if (!pthread) {
-		fprintf(stdout, "You have not joined a session yet.\n");
-		return;
-	}
+	} 
 	int numbytes;
-	// TODO: content of send
-	if ((numbytes = send(socketfd, "list", BUF_SIZE - 1, 0)) == -1) {
+  Packet packet;
+  packet.type = QUERY;
+  packet.size = 0;
+  packetToString(&packet, buf);
+	
+	if ((numbytes = send(socketfd, buf, BUF_SIZE - 1, 0)) == -1) {
 		fprintf(stderr, "client: send\n");
 		return; 
 	}
@@ -262,14 +276,18 @@ void send_text(int socketfd) {
 	if (socketfd == INVALID_SOCKET) {
 		fprintf(stdout, "You have not logged in to any server.\n");
 		return;
-	} else if (!pthread) {
-		fprintf(stdout, "You have not joined a session yet.\n");
-		return;
-	}
+	} else if (!insession) {
+    fprintf(stdout, "You have not joined a session yet.\n");
+    return;
+  } 
 
 	int numbytes;
-	// TODO: content of send
-	if ((numbytes = send(socketfd, "text", BUF_SIZE - 1, 0)) == -1) {
+  Packet packet;
+  packet.type = MESSAGE;
+  strncpy(packet.data, buf, MAX_DATA);
+  packet.size = strlen(packet.data); 
+  packetToString(&packet, buf);	
+	if ((numbytes = send(socketfd, buf, BUF_SIZE - 1, 0)) == -1) {
 		fprintf(stderr, "client: send\n");
 		return; 
 	}
@@ -291,17 +309,18 @@ int main() {
 
 	for (;;) {
 		fgets(buf, BUF_SIZE - 1, stdin);
-		pch = strtok(buf, " ");
+		buf[strcspn(buf, "\r\n")] = 0;
+    pch = strtok(buf, " ");
 		if (strncmp(pch, LOGIN_CMD, LOGIN_CMD_LEN) == 0) {
-			login(pch, &socketfd);
+			login(pch, &socketfd, &receive_thread);
 		} else if (strncmp(pch, LOGOUT_CMD, LOGOUT_CMD_LEN) == 0) {
 			logout(&socketfd, &receive_thread);
 		} else if (strncmp(pch, JOINSESSION_CMD, JOINSESSION_CMD_LEN) == 0) {
-			joinsession(pch, &socketfd, &receive_thread);
+			joinsession(pch, &socketfd);
 		} else if (strncmp(pch, LEAVESESSION_CMD, LEAVESESSION_CMD_LEN) == 0) {
-			leavesession(socketfd, &receive_thread);
+			leavesession(socketfd);
 		} else if (strncmp(pch, CREATESESSION_CMD, CREATESESSION_CMD_LEN) == 0) {
-			createsession(pch, &socketfd, &receive_thread);
+			createsession(pch, &socketfd);
 		} else if (strncmp(pch, LIST_CMD, LIST_CMD_LEN) == 0) {
 			list(socketfd);
 		} else if (strncmp(pch, QUIT_CMD, QUIT_CMD_LEN) == 0) {
@@ -314,3 +333,4 @@ int main() {
 	fprintf(stdout, "You have quit successfully.\n");
 	return 0;
 }
+
